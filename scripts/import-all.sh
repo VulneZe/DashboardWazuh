ad9ed5482b7af6c09c89c7ba28fff935ef92a5d0
VulneZe SteelZe <raveus@proton.me>
2026-04-17 14:12:45 +0200

    Add general SOC dashboards: SOC Overview, Threat Detection, Compliance Monitoring

--- scripts/import-all.sh
+++ scripts/import-all.sh
@@ -8,7 +8,10 @@ DASHBOARDS_DIR="${ROOT_DIR}/dashboards"
 for f in \
   "${DASHBOARDS_DIR}/linux-security-overview.ndjson" \
   "${DASHBOARDS_DIR}/ssh-auth-attacks.ndjson" \
-  "${DASHBOARDS_DIR}/file-integrity-monitoring.ndjson"
+  "${DASHBOARDS_DIR}/file-integrity-monitoring.ndjson" \
+  "${DASHBOARDS_DIR}/soc-overview.ndjson" \
+  "${DASHBOARDS_DIR}/threat-detection.ndjson" \
+  "${DASHBOARDS_DIR}/compliance-monitoring.ndjson"
 do
   "${SCRIPT_DIR}/import-saved-object.sh" "$f"
 done
